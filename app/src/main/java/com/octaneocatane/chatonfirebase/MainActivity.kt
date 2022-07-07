0170e15349e3fb1d0a03179677bee89def3ebe48
Octane Silvio <ruslangibadullin70@gmail.com>
2022-07-07 18:50:08 +0500

    added loading messages to the server, and displaying messages from the server on the screen. Added viewBinding

--- app/src/main/java/com/octaneocatane/chatonfirebase/MainActivity.kt
+++ app/src/main/java/com/octaneocatane/chatonfirebase/MainActivity.kt
@@ -2,16 +2,43 @@ package com.octaneocatane.chatonfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.octaneocatane.chatonfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // Write a message to the database
         val database = Firebase.database
-        val myRef = database.getReference("message").child("Ruslan")
-        myRef.setValue("Hello, World!")
+        val myRef = database.getReference("message")
+        binding.buttonSend.setOnClickListener {
+            myRef.setValue(binding.editMessage.text.toString())
+        }
+        onChangeListener(myRef)
     }
+
+    //в функцию мы передаем путь на котором будем слушать изменения
+    private fun onChangeListener(databaseReference: DatabaseReference) {
+        databaseReference.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\nRuslan: ${snapshot.value.toString()}")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+    }
+
 }
